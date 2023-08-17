2faeed829b0082548705afef9371f042ca4c38e3
Ryan Tyler <rtyler@glam.ac.uk>
2023-08-17 02:27:08 +0100

    Added basic map generation

--- assets/js/level.js
+++ assets/js/level.js
@@ -3,6 +3,7 @@ function level(num, canvasW, canvasH, scale) {
   this.tiles = [];
   this.active = false;
   this.startPos = [0, 0];
+  this.cols = 30;
 
   // Isometric tileSize - Width remains the same, but height is half
   let tileWidth = 16;
@@ -16,23 +17,28 @@ function level(num, canvasW, canvasH, scale) {
   }
 
   this.reset = function(id, scaled) {
+    console.log("RESET");
     this.tiles = [];
     this.dTiles = [];
     mvd = 0;
     let trigger = false;
     let t = 0;
-
+    let maxWater = rndNo(2,5);
+    let water=0;
     // Main level tiles
     let rows = 30;
-    this.cols = 30;
 
+    // Main Loop
     for (r = 0; r < rows; r++) {
       for (c = 0; c < this.cols; c++) {
         let t = 1;
 
-        if(rndNo(0,100)>90){ t=types.AIR }
-        if(rndNo(0,100)>70){ t=types.BRDE }
-        if(rndNo(0,100)>60){ t=types.WTR }
+        //if(rndNo(0,100)>90){ t=types.AIR }
+        // if(rndNo(0,100)>90){ t=types.BRDE }
+        if(rndNo(0,100)>99 && water<maxWater){
+          t=types.WTR;
+          water++;
+        }
 
         // Adjust the xx and yy calculation for isometric positioning
         xx = (c - r) * tileWidth;
@@ -43,9 +49,46 @@ function level(num, canvasW, canvasH, scale) {
         this.tiles.push(tile);
       }
     }
+
+    // Expand Water Areas
+    let changes = [];
+    let maxTiles = this.tiles.length;
+
+    for (let i = 0; i < maxTiles; i++) {
+        let tile = this.tiles[i];
+
+        if (isWater(tile.entity.type)) {
+            for (let r = 0; r < rndNo(3,5); r++) {
+                for (let col = 0; col < rndNo(3,5); col++) {
+                    let pos = getTilePos(tile.row +r, tile.column+col, 30);
+
+                    if (isValidPos(pos, maxTiles)) {
+                        changes.push(pos);
+                    }
+                }
+            }
+        }
+    }
+
+    changes.forEach(pos => {
+        this.tiles[pos].entity.type = 4;
+        this.tiles[pos].entity.setType();
+    });
+  }
+
+  function isValidPos(pos, max) {
+      return pos >= 0 && pos < max;
+  }
+
+  function getTilePos(row, column,totalCols) {
+    return row * totalCols + column;
   }
 
   function isAir(t) {
     return t == types.AIR;
+  }
+
+  function isWater(t) {
+    return t == types.WTR;
   }
 }
